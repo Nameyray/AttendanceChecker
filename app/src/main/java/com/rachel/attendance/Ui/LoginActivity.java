1fd827597595e6b2cb02ac77e6a564b9f71d7334
Nameyray <namibaracheal@gmail.com>
2023-04-13 15:24:10 +0300

    Add the bottom and home menus

--- app/src/main/java/com/rachel/attendance/Ui/LoginActivity.java
+++ app/src/main/java/com/rachel/attendance/Ui/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.rachel.attendance.Ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.rachel.attendance.R;
 
@@ -12,5 +17,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(LoginActivity.this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgot,null);
+        EditText emailBox = dialogView.findViewById(R.id.emailBox);
+
+
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+
+        dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        dialogView.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        if (dialog.getWindow() != null){
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        dialog.show();
+    }
+
+
+    public void Sign(View view) {
+        startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/rachel/attendance/Ui/SplashActivity.java
+++ app/src/main/java/com/rachel/attendance/Ui/SplashActivity.java
@@ -2,13 +2,32 @@ package com.rachel.attendance.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashActivity extends AppCompatActivity {
+import com.rachel.attendance.R;
 
+public class
+SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000; // Time to display the splash screen in milliseconds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app's main activity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // Close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
